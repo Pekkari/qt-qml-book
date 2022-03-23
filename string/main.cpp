d2f445a99e9f4d2b76891ffcfb81d181878bb0bd
José Pekkarinen <jose.pekkarinen@foxhound.fi>
2022-03-23 15:50:02 +0200

    Add first example of qt qml book
    This commit adds the first example listed in
    qt qml 6 book.
    
    Signed-off-by: José Pekkarinen <jose.pekkarinen@foxhound.fi>

---
+++ string/main.cpp
@@ -0,0 +1,17 @@
+// module or class includes
+#include <QtCore>
+
+// text stream is text-codec aware
+QTextStream cout(stdout, QIODevice::WriteOnly);
+
+int main(int argc, char** argv)
+{
+    // avoid compiler warnings
+    Q_UNUSED(argc)
+    Q_UNUSED(argv)
+    QString s1("Paris");
+    QString s2("London");
+    // string concatenation
+    QString s = s1 + " " + s2 + "!";
+    cout << s << Qt::endl;
+}
